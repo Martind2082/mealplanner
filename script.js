f93131ffd4da09aa3933c1b00263fd5c28446e19
Martin <martindao2082@gmail.com>
2023-11-20 00:27:01 -0800

    addded nutritional analysis section

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ let main = document.getElementById('main');
 let reciperesults = document.getElementById('recipe-results');
 let recipes = document.getElementById('recipes');
 
+let nutritionresults = document.getElementById("nutrition-results");
+
 async function getrecipes(val) {
     fetch(`https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&q=${val}`)
     .then(res => res.json())
     .then(data => {
         if (data.hits.length === 0) {
             document.getElementById('popup').style.display = 'flex';
+            document.getElementById('popup').textContent = "No recipes found";
             setTimeout(() => {
                 document.getElementById('popup').style.display = 'none';
             }, 2000);
@@ -95,13 +98,90 @@ async function getrecipes(val) {
 let recipeinput = document.getElementById('recipeinput');
 recipeinput.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-        getrecipes(recipeinput.value)
+        if (recipeinput.value.length === 0) {
+            return;
+        }
+        getrecipes(recipeinput.value);
     }
+})
+document.getElementById('magnifying-glass').addEventListener('click', () => {
+    if (recipeinput.value.length === 0) {
+        return;
+    }
+    getrecipes(recipeinput.value);
 })
 
 //When the back button on the recipe results page is clicked, take user back to home page
-let reciperesultsback = document.getElementById('recipe-results--back');
-reciperesultsback.addEventListener('click', () => {
+let goback = document.getElementsByClassName('goback');
+goback[0].addEventListener('click', () => {
     main.style.display = 'block';
     reciperesults.style.display = 'none';
+})
+
+
+async function getnutrition(val) {
+    fetch(`https://api.edamam.com/api/nutrition-data?app_id=ef5b31be&app_key=a2998f78c1d882d4f9e00f8b124e93af&nutrition-type=cooking&ingr=${val}`)
+    .then(res => res.json())
+    .then(data => {
+        if (Object.keys(data.totalDaily).length === 0) {
+            document.getElementById('popup').style.display = 'flex';
+            document.getElementById('popup').textContent = "No data found";
+            setTimeout(() => {
+                document.getElementById('popup').style.display = 'none';
+            }, 2000);
+            return;
+            return;
+        }
+
+        nutritionresults.style.display = "block";
+
+        document.getElementById("calories-num").textContent = data.calories;
+
+        let nutritioninfo = document.getElementById('nutrition-info');
+
+        let namearray = ["Total Fat", "Saturated Fat", "Trans Fat", "Cholesterol", "Sodium", "Total Carbohydrate", "Dietary Fiber", "Total Sugars", "Includes-Added Sugars", "Protein", "Vitamin D", "Calcium", "Iron", "Potassium"];
+        let gramsarray = ["FAT", "FASAT", "FATRN", "CHOLE", "NA", "CHOCDF", "FIBTG", "SUGAR", "", "PROCNT", "VITD", "CA", "FE", "K"];
+        let percentarray = ["FAT", "FASAT", "", "CHOLE", "NA", "CHOCDF", "FIBTG", "", "", "PROCNT", "VITD", "CA", "FE", "K"];
+
+        for (let i = 0; i < nutritioninfo.children.length; i++) {
+            nutritioninfo.children[i].style.display = "flex";
+            nutritioninfo.children[i].style.justifyContent = "space-between";
+            nutritioninfo.children[i].children[1].style.fontWeight = "bold";
+            if (i === 0 || i === 3 || i === 4 || i === 5 || i === 9) {
+                nutritioninfo.children[i].children[0].style.fontWeight = "bold";
+            }
+
+            let gramsname = gramsarray[i];
+            if (gramsname !== "") {
+                let grams = Math.round(data.totalNutrients[gramsname].quantity * 10) / 10 ;
+                nutritioninfo.children[i].children[0].textContent = `${namearray[i]} ${grams} ${data.totalNutrients[gramsname].unit}`;
+            } else {
+                nutritioninfo.children[i].children[0].textContent = namearray[i];
+                continue;
+            }
+
+            let percentname = percentarray[i];
+            if (percentname !== "") {
+                let percent = Math.round(data.totalDaily[percentname].quantity);
+                nutritioninfo.children[i].children[1].textContent = `${percent} %`;
+            } else {
+                nutritioninfo.children[i].children[1].textContent = "";
+            }
+        }
+
+    })
+}
+document.getElementById("nutrition-button").addEventListener("click", () => {
+    if (document.getElementById("nutrition-textarea").value.length === 0) {
+        return;
+    }
+    let value = document.getElementById("nutrition-textarea").value;
+    let array = [];
+    value = value.split(' ');
+    array.push(value[0]);
+    for (let i = 1; i < value.length; i++) {
+        array.push(`%20${value[i]}`);
+    }
+    array = array.join('');
+    getnutrition(array);
 })
